9074f15ae30e69037fe8b6338c718403b2f228ed
cameronbriggs <98107637+cameronbriggs@users.noreply.github.com>
2022-12-07 22:19:51 -0500

    Final

--- Main.py
+++ Main.py
@@ -8,13 +8,58 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         genre = request.form["genre"]
-        return redirect(url_for("movies", gre=genre))
+        year = request.form["year"]
+        if genre == "Action" and year == "1990's":
+            return render_template("MovieFile.html")
+
+        elif genre == "Action" and year == "2000's":
+            return render_template("Action_2000.html")
+
+        elif genre == "Action" and year == "2010's":
+            return render_template("Action_2010.html")
+
+        elif genre == "Comedy" and year == "1990's":
+            return render_template("Comedy_1990.html")
+
+        elif genre == "Comedy" and year == "2000's":
+            return render_template("Comedy_2000.html")
+
+        elif genre == "Comedy" and year == "2010's":
+            return render_template("Comedy_2010.html")
+
+        elif genre == "Romance" and year == "1990's":
+            return render_template("Romance_1990.html")
+
+        elif genre == "Romance" and year == "2000's":
+            return render_template("Romance_2000.html")
+
+        elif genre == "Romance" and year == "2010's":
+            return render_template("Romance_2010.html")
+
+        elif genre == "Horror" and year == "1990's":
+            return render_template("Horror_1990.html")
+
+        elif genre == "Horror" and year == "2000's":
+            return render_template("Horror_2000.html")
+
+        elif genre == "Horror" and year == "2010's":
+            return render_template("Horror_2010.html")
+
+        elif genre == "Drama" and year == "1990's":
+            return render_template("Drama_1990.html")
+
+        elif genre == "Drama" and year == "2000's":
+            return render_template("Drama_2000.html")
+
+        elif genre == "Drama" and year == "2010's":
+            return render_template("Drama_2010.html")
+        
     else:
         return render_template("index.html")
 
-@app.route("/<gre>")
-def movies(gre):
-    return f"<h1>{gre}</h1>"
+#@app.route("/<gre>")
+#def movies(gre):
+    #return render_template("index.html")
 
 
 
